2997e99e75660aaf49e51b780f5de27cbdd0caa0
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2020-12-02 10:59:10 -0500

    removed unneeded code

--- Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace SAEBRecommender.Models
-{
-    public class TodoItem
-    {
-        public long ID { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using SAEBRecommender.Models;
 
 namespace SAEBRecommender
 {
@@ -28,7 +19,6 @@ namespace SAEBRecommender
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
